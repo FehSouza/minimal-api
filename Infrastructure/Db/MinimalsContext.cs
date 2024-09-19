f02cdfcd774103b1da51e786d9cc0966171f570c
FehSouza <fernanda.souza142010@gmail.com>
2024-09-18 21:15:16 -0300

    feat: administrator seed creation

--- Infrastructure/Db/MinimalsContext.cs
+++ Infrastructure/Db/MinimalsContext.cs
@@ -5,6 +5,21 @@ namespace minimals_api.Infrastructure.Db
 {
 	public class MinimalsContext(DbContextOptions<MinimalsContext> options) : DbContext(options)
 	{
+		// O método OnModelCreating é chamado quando o BD está sendo configurado (criado ou atualizado), inserindo um registro padrão (ou seed data) na tabela
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Administrator>().HasData(
+				new Administrator
+				{
+					// O Id foi definido como -1 para garantir que esse registro seed tenha um ID único e que não entre em conflito com registros futuros do BD
+					Id = -1,
+					Email = "admin@teste.com.br",
+					Password = "123456",
+					Profile = "Admin"
+				}
+			);
+		}
+
 		public DbSet<Administrator> Administrators { get; set; }
 	}
 }
--- Migrations/20240919000715_SeedAdministrator.Designer.cs
+++ Migrations/20240919000715_SeedAdministrator.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using minimals_api.Infrastructure.Db;
 
@@ -10,9 +11,11 @@ using minimals_api.Infrastructure.Db;
 namespace minimals_api.Migrations
 {
     [DbContext(typeof(MinimalsContext))]
-    partial class MinimalsContextModelSnapshot : ModelSnapshot
+    [Migration("20240919000715_SeedAdministrator")]
+    partial class SeedAdministrator
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
